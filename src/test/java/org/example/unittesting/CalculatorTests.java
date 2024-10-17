7f29672ec2d92c1f832fdf4397d44209d74d99a2
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-17 10:33:03 -0500

    Implementacion de métodos test para calculadora

--- src/test/java/org/example/unittesting/CalculatorTests.java
+++ src/test/java/org/example/unittesting/CalculatorTests.java
@@ -29,6 +29,129 @@ public class CalculatorTests {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void subtract() {
+        // Arrange
+        Double a = 3.0;
+        Double b = 2.0;
+        Double expected = 1.0;
+
+        // Act
+        Double actual = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void multiply() {
+        // Arrange
+        Double a = 3.0;
+        Double b = 2.0;
+        Double expected = 6.0;
+
+        // Act
+        Double actual = calculator.multiply(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void divide() {
+        // Arrange
+        Double a = 8.0;
+        Double b = 2.0;
+        Double expected = 4.0;
+
+        // Act
+        Double actual = calculator.divide(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void square() {
+        // Arrange
+        Double a = 3.0;
+        Double expected = 9.0;
+
+        // Act
+        Double actual = calculator.square(a);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void squareRoot() {
+        // Arrange
+        Double a = 16.0;
+        Double expected = 4.0;
+
+        // Act
+        Double actual = calculator.squareRoot(a);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void modulus() {
+        // Arrange
+        Double a = 8.0;
+        Double b = 3.0;
+        Double expected = 2.0;
+
+        // Act
+        Double actual = calculator.modulus(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void power() {
+        // Arrange
+        Double a = 3.0;
+        Double b = 4.0;
+        Double expected = 81.0;
+
+        // Act
+        Double actual = calculator.power(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void squareRootFailed() {
+        // Arrange
+        Double a = 9.0;
+        Double expected = 2.0;
+
+        // Act
+        Double actual = calculator.squareRoot(a);
+
+        // Assert
+        assert(actual != expected);
+    }
+
+    @Test
+    void modulusFailed() {
+        // Arrange
+        Double a = 4.0;
+        Double b = 2.0;
+        Double expected = 1.0;
+
+        // Act
+        Double actual = calculator.modulus(a, b);
+
+        // Assert
+        assert(expected != actual);
+    }
+
 
 
 }
