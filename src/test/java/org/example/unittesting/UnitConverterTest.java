5039f9965e4c495dcfbd54898c1147143c5e87c4
Sebastian <sanchezse@javeriana.edu.co>
2024-10-17 10:38:31 -0500

    Implementacion de metodos de unidades de TEST y correcion en metodos

--- src/test/java/org/example/unittesting/UnitConverterTest.java
+++ src/test/java/org/example/unittesting/UnitConverterTest.java
@@ -23,4 +23,61 @@ public class UnitConverterTest {
         // Assert
         assert expected.equals(actual);
     }
+
+    void fahrenheitToCelsius() {
+        // Arrange
+        Double fahrenheit = 32.0;
+        Double expected = 0.0;
+
+        // Act
+        Double actual = unitConverter.fahrenheitToCelsius(fahrenheit);
+
+        // Assert
+        assert expected.equals(actual);
+    }
+    void kilometerToMile() {
+        // Arrange
+        Double kilometer = 1.0;
+        Double expected = 0.621371;
+
+        // Act
+        Double actual = unitConverter.kilometerToMile(kilometer);
+
+        // Assert
+        assert expected.equals(actual);
+    }
+    void mileToKilometer() {
+        // Arrange
+        Double mile = 1.0;
+        Double expected = 1.609;
+
+        // Act
+        Double actual = unitConverter.mileToKilometer(mile);
+
+        // Assert
+        assert expected.equals(actual);
+    }
+
+void kilogramToPounds() {
+    // Arrange
+    Double kilogram = 1.0;
+    Double expected = 2.205;
+
+    // Act
+    Double actual = unitConverter.kilogramToPound(kilogram);
+
+    // Assert
+    assert expected.equals(actual);
+}
+void poundsToKilogram() {
+    // Arrange
+    Double pound = 2.205;
+    Double expected = 1.0;
+
+    // Act
+    Double actual = unitConverter.poundToKilogram(pound);
+
+    // Assert
+    assert expected.equals(actual);
+}
 }
